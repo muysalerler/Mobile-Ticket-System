07326b39b96e3685cb74e4b40fba74535baa481d
muysalerler <muysalerler@gmail.com>
2025-02-25 16:44:38 +0300

    guncel

--- app/src/main/java/com/example/part2/MainActivity.java
+++ app/src/main/java/com/example/part2/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageViewMovie;
     private RadioGroup radioGroupTicketType, radioGroupPayment;
     private RadioButton radioMultiple, radioSingle;
+    private LinearLayout layoutTicketCount;
     private TextView tvTicketCount;
     private SeekBar seekBarTickets;
     private EditText etName;
@@ -32,17 +33,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        spinnerMovies = findViewById(R.id.spinnerMovies);
-        imageViewMovie = findViewById(R.id.imageViewMovie);
+
+        spinnerMovies      = findViewById(R.id.spinnerMovies);
+        imageViewMovie     = findViewById(R.id.imageViewMovie);
         radioGroupTicketType = findViewById(R.id.radioGroupTicketType);
-        radioMultiple = findViewById(R.id.radioMultiple);
-        radioSingle = findViewById(R.id.radioSingle);
-        tvTicketCount = findViewById(R.id.tvTicketCount);
-        seekBarTickets = findViewById(R.id.seekBarTickets);
-        radioGroupPayment = findViewById(R.id.radioGroupPayment);
-        etName = findViewById(R.id.etName);
-        buttonFinish = findViewById(R.id.buttonFinish);
-        buttonClear = findViewById(R.id.buttonClear);
+        radioMultiple      = findViewById(R.id.radioMultiple);
+        radioSingle        = findViewById(R.id.radioSingle);
+        layoutTicketCount  = findViewById(R.id.layoutTicketCount);
+        tvTicketCount      = findViewById(R.id.tvTicketCount);
+        seekBarTickets     = findViewById(R.id.seekBarTickets);
+        radioGroupPayment  = findViewById(R.id.radioGroupPayment);
+        etName             = findViewById(R.id.etName);
+        buttonFinish       = findViewById(R.id.buttonFinish);
+        buttonClear        = findViewById(R.id.buttonClear);
+
         movieNames = getResources().getStringArray(R.array.movies_array);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
@@ -52,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
         );
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerMovies.setAdapter(adapter);
+
         spinnerMovies.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -60,35 +65,31 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onNothingSelected(AdapterView<?> parent) { }
         });
-        radioGroupTicketType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+        radioGroupTicketType.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener(){
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 if (checkedId == R.id.radioMultiple) {
-                    tvTicketCount.setVisibility(View.VISIBLE);
-                    seekBarTickets.setVisibility(View.VISIBLE);
+                    layoutTicketCount.setVisibility(View.VISIBLE);
                 } else {
-                    tvTicketCount.setVisibility(View.GONE);
-                    seekBarTickets.setVisibility(View.GONE);
+                    layoutTicketCount.setVisibility(View.GONE);
                 }
             }
         });
 
-        seekBarTickets.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        seekBarTickets.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 int count = progress + 1;
                 tvTicketCount.setText("Ticket Count (" + count + ")");
             }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) { }
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) { }
+            @Override public void onStartTrackingTouch(SeekBar seekBar) { }
+            @Override public void onStopTrackingTouch(SeekBar seekBar) { }
         });
 
-        buttonFinish.setOnClickListener(new View.OnClickListener() {
+        buttonFinish.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 String name = etName.getText().toString().trim();
                 if (name.isEmpty()) {
                     Toast.makeText(MainActivity.this,
@@ -96,6 +97,7 @@ public class MainActivity extends AppCompatActivity {
                             Toast.LENGTH_SHORT).show();
                     return;
                 }
+
                 int moviePos = spinnerMovies.getSelectedItemPosition();
                 String selectedMovie = movieNames[moviePos];
 
@@ -106,34 +108,38 @@ public class MainActivity extends AppCompatActivity {
                 if (radioMultiple.isChecked()) {
                     ticketCount = seekBarTickets.getProgress() + 1;
                 }
+
                 AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setIcon(R.drawable.sinema);
+                builder.setIcon(R.drawable.sinema); // res/drawable/cinema.png
                 builder.setTitle(getString(R.string.ticket_details_title));
+
                 String msg = "Movie : " + selectedMovie
                         + "\nPayment : " + paymentMethod
                         + "\nTicket Count : " + ticketCount
                         + "\nYour Name: " + name;
 
                 builder.setMessage(msg);
-                builder.setPositiveButton(getString(R.string.close_button), new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.dismiss();
-                    }
-                });
+
+                builder.setPositiveButton(getString(R.string.close_button),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
 
                 builder.show();
             }
         });
-        buttonClear.setOnClickListener(new View.OnClickListener() {
+
+        buttonClear.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
                 spinnerMovies.setSelection(0);
                 imageViewMovie.setImageResource(movieImages[0]);
                 radioGroupTicketType.clearCheck();
-                tvTicketCount.setVisibility(View.GONE);
+                layoutTicketCount.setVisibility(View.GONE);
                 seekBarTickets.setProgress(0);
-                seekBarTickets.setVisibility(View.GONE);
                 ((RadioButton)findViewById(R.id.radioPaypal)).setChecked(true);
                 etName.setText("");
             }
